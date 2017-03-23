b62004cca0612224882fa820663ac43ef5cd74c4
ulutasalkan <ulutasalkan@hotmail.com>
2017-03-24 00:19:54 +0300

    RunCode

---
+++ App.java
@@ -0,0 +1,13 @@
+package Homework_Java_018;
+
+import java.util.ArrayList;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		XMLDataFormat data=new XMLDataFormat();
+		data.display(data.createDataStructure());
+	}
+
+}
